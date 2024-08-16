2000c9162f174a0c9eef7b9d9618fb4b9ef47e43
Fabio <fabio.dantas@flexmanager.com>
2024-08-16 13:06:40 +0100

    Initial Commit

---
+++ FirestoreGeoSwift/FirestoreGeoSwift.h
@@ -0,0 +1,18 @@
+//
+//  FirestoreGeoSwift.h
+//  FirestoreGeoSwift
+//
+//  Created by Fabio Dantas on 16/08/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirestoreGeoSwift.
+FOUNDATION_EXPORT double FirestoreGeoSwiftVersionNumber;
+
+//! Project version string for FirestoreGeoSwift.
+FOUNDATION_EXPORT const unsigned char FirestoreGeoSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirestoreGeoSwift/PublicHeader.h>
+
+
